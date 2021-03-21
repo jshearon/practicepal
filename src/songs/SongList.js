b52d7034230f44289655177bd81db0c4a1b9a24f
Jonathan Shearon <jonathan.shearon@gmail.com>
2021-03-21 14:11:05 -0500

    song form complete

--- src/songs/SongList.js
+++ src/songs/SongList.js
@@ -1,11 +1,17 @@
 import React, { useContext, useEffect, useState } from "react"
 import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { Typography, Grid, CircularProgress, Card, CardContent, CardHeader, Button, Box, Paper } from '@material-ui/core'
+import { Typography, Grid, CircularProgress, Card, CardContent, CardHeader, Button, Box, Paper, Dialog } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 import { SongContext } from '../Songs/SongProvider'
+import { SongForm } from './SongForm'
 import { Document, Page, pdfjs } from 'react-pdf';
 import './Songs.scss'
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import IconButton from '@material-ui/core/IconButton';
+import PageviewIcon from '@material-ui/icons/Pageview';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
 
 
@@ -20,27 +26,49 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export const Dashboard = (props) => {
-
-  const history = useHistory()
-
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
-
-  const { userSections, getUserSections } = useContext(SectionContext)
+export const SongList = (props) => {
 
   const classes = useStyles();
 
+  const history = useHistory()
+
+  const { userSongs, getUserSongs, getSingleSong, setSingleSong } = useContext(SongContext)
+
+  const [pageNumber, setPageNumber] = useState(1);
+  const [openSongForm, setOpenSongForm] = useState(false);
+
+
   useEffect(() => {
-    getUserSections()
+    getUserSongs()
   }, [])
+
+  const handleSongFormClick = () => {
+    setOpenSongForm(!openSongForm)
+  }
+
+  const handleEditSongFormClick = (id) => {
+    getSingleSong(id)
+    setOpenSongForm(!openSongForm)
+  }
+
+  const handleDeleteSongFormClick = (id) => {
+    return fetch(`http://127.0.0.1:8000/songs/${id}`, {
+              method: "DELETE",
+              headers: {
+                "Authorization": `Token ${localStorage.getItem("pp_token")}`
+              },
+          })
+              .then(res => res.json())
+              .then(res => {
+                getUserSongs()
+              })
+  }
 
   return ( 
     <div className={classes.root}>
+        <Dialog open={openSongForm} onClose={openSongForm}>
+          <SongForm handleSongFormClick={handleSongFormClick} />
+       </Dialog>
        <Grid 
           container 
           className={classes.root} 
@@ -49,56 +77,51 @@ export const Dashboard = (props) => {
         >
 
 
-          <Grid item xs={12} align="center">
-            <Typography variant="h4">Dashboard</Typography>
+          <Grid item xs={12} align="center" className="m-3">
+            <Typography variant="h4">Works</Typography>
           </Grid>
+          <Grid item xs={12} align="center">
+                <IconButton onClick={() => {
+                  setSingleSong({
+                    title: "",
+                    composer: "",
+                    instrument: "init",
+                    pdf: ""
+                  })
+                  setOpenSongForm(true)}}>
+                  <AddCircleIcon fontSize="large" />
+                </IconButton>
+          </Grid>
+          <Grid item xs={12} align="center">
           {
-              userSections.map(section => {
+           userSongs.map(song => {
                 return (
-              <Grid item xs={11} sm={8}>
-                <Card key={section.id} raised>
+                <Card key={song.id} raised style={{display: 'inline-block'}}>
                   <CardContent>
-                  <Paper elevation={3}>
-                    <Grid container spacing={0} justify="center">
-                      <Grid item xs={6} align="left" className="d-flex flex-column justify-content-center">
                         <Typography variant="h5">
-                          <strong>{section.song.title}</strong>
+                          {song.title}
                         </Typography>
-                      </Grid>
-                      <Grid item xs={6} align="right" className="d-flex flex-column justify-content-center">
-                        <Typography variant="h5">{section.label}</Typography>
-                      </Grid>
-                    </Grid>
-                  </Paper>
-                    <Grid container spacing={0} justify="flex-start" alignContent="flex-start"> 
-                        <Grid item xs={1} align="center" style = {{minWidth: "120px"}}>
-                            <Document file={section.song.pdf} >
-                              <Page pageNumber={1} height={150} />
-                            </Document>
-                        </Grid>
-                        <Grid item xs align="center">
-                          <CircularProgressWithLabel value={section.percent_complete} size={150} />
-                      </Grid>
-                      <Grid 
-                        item 
-                        xs={1} 
-                        style = {{minWidth: "120px"}} 
-                        align="center"
-                        className="d-flex flex-column justify-content-center"
-                      >
-                            <Button 
-                              size="small" 
-                              color="primary" 
-                              variant="contained"
-                              onClick={() => history.push(`/section/${section.id}`)}>Practice</Button>
-                      </Grid>
-                    </Grid> 
+                        <Typography variant="overline">
+                          {song.composer}
+                        </Typography>
+                        <Document file={song.pdf} >
+                          <Page pageNumber={1} height={150} />
+                        </Document>
+                        <IconButton onClick={() => history.push(`/song/${song.id}`)}>
+                          <PageviewIcon />
+                        </IconButton>
+                        <IconButton onClick={() => handleEditSongFormClick(song.id)}>
+                          <EditIcon />
+                        </IconButton>
+                        <IconButton onClick={() => handleDeleteSongFormClick(song.id)}>
+                          <DeleteForeverIcon />
+                        </IconButton>
                   </CardContent>
                 </Card>
-                </Grid>
                 )
               })
           }
+          </Grid>
       </Grid>
     </div>
   )
